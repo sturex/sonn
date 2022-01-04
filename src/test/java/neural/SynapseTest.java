5c9f607b8f758078aee016fc4cb837c2df946c8f
sturex <turex2019@gmail.com>
2022-01-04 19:22:39 +0300

    Remove outdated Synapse tests

--- src/test/java/neural/SynapseTest.java
+++ src/test/java/neural/SynapseTest.java
@@ -12,40 +12,4 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("Synapse tests")
 class SynapseTest {
 
-
-    @Test
-    @DisplayName("Synapse correctly accepts forward flow")
-    void forwardFlowExcSynapseTest() {
-
-        Receptor receptor = new Receptor(() -> true);
-        Effector effector = new Effector(() -> {
-        });
-
-        Arrays.stream(Synapse.Type.values())
-                .map(t -> new Synapse<>(receptor, effector, t))
-                .forEach(s -> Arrays.stream(Flow.values())
-                        .forEach(flow -> {
-                            s.acceptForward(flow);
-                            assertEquals(flow, s.getForward());
-                            assertEquals(Flow.STILL, s.getBackward());
-                        }));
-    }
-
-    @Test
-    @DisplayName("Synapse correctly accepts backward flow")
-    void backwardFlowExcSynapseTest() {
-
-        Receptor receptor = new Receptor(() -> true);
-        Effector effector = new Effector(() -> {
-        });
-
-        Arrays.stream(Synapse.Type.values())
-                .map(t -> new Synapse<>(receptor, effector, t))
-                .forEach(s -> Arrays.stream(Flow.values())
-                        .forEach(flow -> {
-                            s.acceptBackward(flow);
-                            assertEquals(flow, s.getBackward());
-                            assertEquals(Flow.STILL, s.getForward());
-                        }));
-    }
 }
