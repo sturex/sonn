8cf4597cec22348cf8d08985818504639def7a3f
sturex <turex2019@gmail.com>
2022-01-05 13:44:18 +0300

    Implement synapse model with memory

--- src/main/java/neural/Synapse.java
+++ src/main/java/neural/Synapse.java
@@ -4,20 +4,39 @@ import core.*;
 
 public class Synapse<T extends Node<?, ?>, U extends Node<?, ?>> extends Edge<T, U> implements FlowConsumer, FlowSupplier {
 
+    public enum Type {
+        INHIBITORY, EXCITATORY
+    }
+
+    private final Type type;
+
     Synapse(T input, U output, Type type) {
         super(input, output);
         this.type = type;
-    }
-
-    public enum Type {
-        INHIBITORY, EXCITATORY
+        setBackwardFlow(Flow.RUN);
     }
 
     public Type getType() {
         return type;
     }
 
-    private final Type type;
+    @Override
+    public void acceptForward(Flow flow) {
+        if (getBackward() == Flow.RUN && type == Type.EXCITATORY) {
+            super.acceptForward(Flow.RUN);
+        } else {
+            super.acceptForward(flow);
+        }
+    }
+
+    @Override
+    public void acceptBackward(Flow flow) {
+        if (getForward() == Flow.RUN && type == Type.EXCITATORY) {
+            super.acceptBackward(flow);
+        } else {
+            super.acceptBackward(flow);
+        }
+    }
 
     @Override
     public String toString() {
