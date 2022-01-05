486be5d8250c77625e1e58b3ed4c2fed19422bc7
sturex <turex2019@gmail.com>
2022-01-05 13:46:32 +0300

    Reject justCreated model

--- src/main/java/neural/Neuron.java
+++ src/main/java/neural/Neuron.java
@@ -7,8 +7,6 @@ import java.util.List;
 
 public class Neuron extends Node<Synapse<Node<?, ?>, Node<?, ?>>, Synapse<Node<?, ?>, Node<?, ?>>> {
 
-    private boolean justCreated = true;
-
     @Override
     public Flow convergeForward(List<Synapse<Node<?, ?>, Node<?, ?>>> ts) {
         return Network.convergeForward(ts);
@@ -16,12 +14,7 @@ public class Neuron extends Node<Synapse<Node<?, ?>, Node<?, ?>>, Synapse<Node<?
 
     @Override
     public Flow convergeBackward(List<Synapse<Node<?, ?>, Node<?, ?>>> us) {
-        if (justCreated) {
-            justCreated = false;
-            return Flow.RUN;
-        } else {
-            return Network.convergeBackward(us);
-        }
+        return Network.convergeBackward(us);
     }
 
     @Override
