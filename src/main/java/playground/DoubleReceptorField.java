ff4b0066d4f53332f25b86d88ff306c1697a9945
sturex <turex2019@gmail.com>
2022-01-15 17:46:08 +0300

    Fix

--- src/main/java/playground/DoubleReceptorField.java
+++ src/main/java/playground/DoubleReceptorField.java
@@ -15,7 +15,7 @@ public class DoubleReceptorField {
     private static final int lowerBound = -100;
     private static final int upperBound = 100;
     private static final int bCount = 3;
-    private static final int maxNeuronSize = 100;
+    private static final int maxNeuronSize = 400;
 
     public static void main(String[] args) throws InterruptedException {
         Network network = new Network(maxNeuronSize);
@@ -26,10 +26,10 @@ public class DoubleReceptorField {
         network.addDoubleReception(() -> d, new Bounds(lowerBound, upperBound), bCount);
 
         while (true) {
-            network.tick();
             d = lowerBound + random.nextDouble() * (upperBound - lowerBound);
+            network.tick();
             System.out.println(d);
-            Thread.sleep(300);
+            Thread.sleep(100);
         }
     }
 
