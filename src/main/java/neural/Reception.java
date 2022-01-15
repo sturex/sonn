7cb20d6272d44a1696351f720c69708d629f8933
sturex <turex2019@gmail.com>
2022-01-15 17:11:34 +0300

    Simplify

--- src/main/java/neural/Reception.java
+++ src/main/java/neural/Reception.java
@@ -8,8 +8,6 @@ import java.util.function.BooleanSupplier;
 public class Reception implements FlowSupplier {
     private final BooleanSupplier booleanSupplier;
 
-    private Flow forwardFlow = Flow.STILL;
-
     public Reception(BooleanSupplier booleanSupplier) {
         this.booleanSupplier = booleanSupplier;
     }
@@ -21,7 +19,6 @@ public class Reception implements FlowSupplier {
 
     @Override
     public Flow getForward() {
-        forwardFlow = Flow.from(booleanSupplier);
-        return forwardFlow;
+        return Flow.from(booleanSupplier);
     }
 }
