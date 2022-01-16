9026dbff8a9df53cc36e12f58268a5eb103b6e63
sturex <turex2019@gmail.com>
2022-01-17 00:33:37 +0300

    Simplify

--- src/main/java/playground/DoubleReceptorField.java
+++ src/main/java/playground/DoubleReceptorField.java
@@ -3,7 +3,6 @@ package playground;
 import neural.Bounds;
 import neural.Network;
 import vis.GraphStreamDynamicLayout;
-import vis.GraphStreamStaticLayout;
 import vis.LayoutAdapter;
 
 import java.util.Random;
@@ -12,21 +11,21 @@ public class DoubleReceptorField {
 
     private static final Random random = new Random();
     private static double d = 0.;
-    private static final int lowerBound = -100;
-    private static final int upperBound = 100;
+    private static final int lowerBound = 0;
+    private static final int upperBound = 1;
     private static final int bCount = 3;
-    private static final int maxNeuronSize = 400;
+    private static final int maxNeuronSize = 100;
 
     public static void main(String[] args) throws InterruptedException {
         Network network = new Network(maxNeuronSize);
 
-        network.addListener(new LayoutAdapter(new GraphStreamStaticLayout()));
+//        network.addListener(new LayoutAdapter(new GraphStreamStaticLayout()));
         network.addListener(new LayoutAdapter(new GraphStreamDynamicLayout()));
 
         network.addDoubleReception(() -> d, new Bounds(lowerBound, upperBound), bCount);
 
         while (true) {
-            d = lowerBound + random.nextDouble() * (upperBound - lowerBound);
+            d = random.nextDouble();
             network.tick();
             System.out.println(d);
             Thread.sleep(100);
