a285f9a71d6ee849055cfb0e15d0db45556c2e96
sturex <turex2019@gmail.com>
2022-01-17 00:32:41 +0300

    Another approach to evaluate outlier estimation

--- src/main/java/playground/OutlierDetectionPlayground.java
+++ src/main/java/playground/OutlierDetectionPlayground.java
@@ -10,29 +10,39 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class OutlierDetectionPlayground {
-    private static final int lowerBound = 0;
+    private static final int lowerBound = -100;
     private static final int upperBound = 100;
     public static final Bounds bounds = new Bounds(lowerBound, upperBound);
-    private static final int bCount = 10;
-    private static final int maxNeuronSize = 10000;
+    private static final int bCount = 100;
+    private static final int maxNeuronSize = 2000;
     public static final String delimiter = ";";
     private static int tdidx = 0;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         StringBuilder sb = new StringBuilder();
         double[][] trafficData = Util.readAsDoubleArray(Path.of("src/main/resources/data/traffic.csv"), delimiter);
 
         Network network = new Network(maxNeuronSize);
 
+//        network.addListener(new LayoutAdapter(new GraphStreamStaticLayout()));
+//        network.addListener(new LayoutAdapter(new GraphStreamDynamicLayout()));
+
         network.addListener(new NetworkEventsListener() {
             @Override
             public void onDeadendNodesDetected(List<Node<?, ?>> deadendNodes, int maxDeadendNeuronCount) {
-                sb.append(deadendNodes.stream().map(n -> maxDeadendNeuronCount + ";" + String.valueOf(n.getId())).collect(Collectors.joining(delimiter)));
+                List<Node<?, ?>> nodes = deadendNodes.stream().sorted(Comparator.comparingInt(Node::getId)).collect(Collectors.toList());
+                double average = deadendNodes.stream().mapToInt(Node::getId).summaryStatistics().getAverage();
+                sb
+                        .append(1. - (double) nodes.get(nodes.size() - 1).getId() / (double) maxDeadendNeuronCount).append(delimiter)
+                        .append(1. - average / (double) maxDeadendNeuronCount).append(delimiter)
+                        .append(maxDeadendNeuronCount).append(delimiter)
+                        .append(deadendNodes.stream().map(n -> String.valueOf(n.getId())).sorted().collect(Collectors.joining(delimiter)));
             }
         });
 
@@ -40,13 +50,14 @@ public class OutlierDetectionPlayground {
         network.addDoubleReception(() -> trafficData[tdidx][1], bounds, bCount);
         network.addDoubleReception(() -> trafficData[tdidx][2], bounds, bCount);
 
-        while (tdidx < 5000) {
+        while (tdidx < trafficData.length - 1 && tdidx < 5000) {
             tdidx++;
             sb.append(tdidx).append(delimiter);
             sb.append(Arrays.stream(trafficData[tdidx]).mapToObj(String::valueOf).collect(Collectors.joining(delimiter))).append(delimiter);
             network.tick();
             sb.append("\n");
             System.out.println(tdidx);
+//            Thread.sleep(300);
         }
 
         Files.write(Path.of("src/main/resources/logs/traffic_output.csv"), sb.toString().getBytes());
